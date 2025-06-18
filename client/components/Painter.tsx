477f81d6d6774b09df226f88a1f7ff96acd277d0
Sasha Chambers <sashachambers10.12@gmail.com>
2025-06-18 22:16:40 +1200

    Made a functional color picker

--- client/components/Painter.tsx
+++ client/components/Painter.tsx
@@ -1,16 +1,30 @@
-//It will read the domain.
-//Get what ever number the domain says
-//Use the number to display a css grid thats interactible
+import { useState } from 'react'
+
 type Props = {
   goBack: any
 }
 
+// It will display a grid based on what button size is press
+// It will display a tool section for possible tools i may make
+
 function Painter({ goBack }: Props) {
+  const [selectedColor, setSelectedColor] = useState('#000000') //Sets the default color to black
+  const handleColorChange = (event: any) => {
+    setSelectedColor(event.target.value) // when activated it will change the color to whatever thing updated it
+  }
+
   return (
     <div className="center">
       <div className="container">
         <div className="tools">
-          <h1>Toools placement</h1>
+          <h1>Toools placement</h1>{' '}
+          {/* I'll place the tools in its own separate file*/}
+          <input
+            type="color"
+            id="color-picker"
+            value={selectedColor}
+            onChange={handleColorChange}
+          />
         </div>
         <div className="canvas">
           <h2>Grid placement</h2>
